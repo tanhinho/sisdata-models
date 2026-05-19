5e2ec5089bb91d67352ed2aa291c7edcc2404b6a
tanhinho <115641400+tanhinho@users.noreply.github.com>
2026-05-19 17:41:46 +0100

    Add lstm tests

---
+++ tests/lstm/test_lstm_model.py
@@ -0,0 +1,31 @@
+import pytest
+
+
+def test_data_loading():
+    """Test that training data can be loaded correctly."""
+    # TODO: Implement this test
+    pass
+
+
+def test_preprocessor_loading():
+    """Test that the preprocessor can be loaded."""
+    # TODO: Implement this test
+    pass
+
+
+def test_model_training():
+    """Test that the model can be trained without errors."""
+    # TODO: Implement this test
+    pass
+
+
+def test_model_prediction():
+    """Test that the trained model can make predictions."""
+    # TODO: Implement this test
+    pass
+
+
+def test_model_performance():
+    """Test that the model achieves reasonable performance."""
+    # TODO: Implement this test
+    pass
