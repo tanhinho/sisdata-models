fb8a897e1a2e791bb11d977d29d50c271bf17dec
tanhinho <115641400+tanhinho@users.noreply.github.com>
2026-08-05 18:56:48 +0100

    Add random forest model

---
+++ training/models/random_forest_model.py
@@ -0,0 +1,65 @@
+import numpy as np
+from typing import Dict, Tuple, Optional
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+from datasets.base_dataset import BaseDataset
+from models.base_model import BaseModel
+
+
+class RandomForestModel(BaseModel):
+    NAME = "random_forest"
+
+    def __init__(
+        self,
+        dataset: BaseDataset,
+        is_optimizing: bool = False,
+        n_estimators: int = 100,
+        max_depth: Optional[int] = None,
+        seq_length: int = 32,
+    ):
+        super().__init__(dataset=dataset, is_optimizing=is_optimizing)
+
+        self.seq_length = seq_length
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+
+        self.model = RandomForestRegressor(n_estimators=self.n_estimators, max_depth=self.max_depth)
+
+    def forward(self, x):
+        # x: (batch, seq_len, features)
+        # RandomForest predicts on flattened sequence
+        if hasattr(x, "detach"):
+            x = x.detach().cpu().numpy()
+        x_flat = x.reshape(x.shape[0], -1)
+        preds = self.model.predict(x_flat)
+        return preds, None, None
+
+    def _fit_and_evaluate_impl(self, df_train, df_test) -> Tuple[float, Dict[str, object]]:
+        # Prepare data using dataset helper
+        X_train, y_train = self.dataset.create_sequences(
+            df_train, self.seq_length, self.FORECAST_HORIZON, self.device
+        )
+        X_test, y_test = self.dataset.create_sequences(
+            df_test, self.seq_length, self.FORECAST_HORIZON, self.device
+        )
+
+        X_train_np = X_train.cpu().numpy().reshape(X_train.shape[0], -1)
+        y_train_np = y_train.cpu().numpy()
+        X_test_np = X_test.cpu().numpy().reshape(X_test.shape[0], -1)
+        y_test_np = y_test.cpu().numpy()
+
+        # Fit sklearn model
+        self.model.fit(X_train_np, y_train_np)
+
+        preds = self.model.predict(X_test_np)
+        val_loss = float(mean_squared_error(y_test_np, preds))
+
+        params = {
+            "seq_length": self.seq_length,
+            "n_estimators": self.n_estimators,
+            "max_depth": self.max_depth,
+        }
+
+        return val_loss, params
---
+++ training/optimizers/random_forest_optimizer.py
@@ -0,0 +1,27 @@
+from typing import Any
+import optuna
+
+from models.random_forest_model import RandomForestModel
+from optimizers.base_optimizer import BaseOptimizer
+
+
+class RandomForestOptimizer(BaseOptimizer):
+    """Optuna optimizer for RandomForestModel."""
+    MODEL = RandomForestModel
+
+    def _objective_impl(self, trial: optuna.Trial) -> float:
+        # hyperparameter search space
+        n_estimators = trial.suggest_int("n_estimators", 50, 300)
+        max_depth = trial.suggest_int("max_depth", 3, 30)
+        seq_length = trial.suggest_int("seq_length", 5, 15)
+
+        model = self.MODEL(
+            dataset=self.dataset,
+            is_optimizing=True,
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            seq_length=seq_length,
+        )
+
+        val_loss = model.fit_and_evaluate(run_name=f"optuna_trial_{trial.number}")
+        return val_loss
