16e13da23768f86c81769184410e1aa35bc0f598
tanhinho <115641400+tanhinho@users.noreply.github.com>
2026-08-05 18:58:20 +0100

    Update init

--- training/optimizers/__init__.py
+++ training/optimizers/__init__.py
@@ -1,4 +1,14 @@
 from .base_optimizer import BaseOptimizer
 from .lstm_optimizer import LSTMOptimizer
+from .tcn_optimizer import TCNOptimizer
+from .random_forest_optimizer import RandomForestOptimizer
+from .transformer_optimizer import TransformerOptimizer
 
-__all__ = ["BaseOptimizer", "LSTMOptimizer"]
+
+__all__ = [
+    "BaseOptimizer",
+    "LSTMOptimizer",
+    "TCNOptimizer",
+    "RandomForestOptimizer",
+    "TransformerOptimizer",
+]
