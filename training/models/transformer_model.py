f06e4fe4a0a2efe706e7627525769674737944bd
tanhinho <115641400+tanhinho@users.noreply.github.com>
2026-08-07 16:47:06 +0100

    Revert batch_first value

--- training/models/transformer_model.py
+++ training/models/transformer_model.py
@@ -62,7 +62,7 @@ class TransformerModel(BaseModel):
             num_decoder_layers=num_layers,
             dim_feedforward=dim_feedforward,
             dropout=dropout,
-            batch_first=True,
+            batch_first=False,
         )
 
         # map decoder outputs (per time-step) to a single scalar prediction
