6b3939054dc394998dcf82745e89703acd4d2a16
tanhinho <115641400+tanhinho@users.noreply.github.com>
2026-08-07 15:43:39 +0100

    Fix dynamic value space error

--- training/optimizers/transformer_optimizer.py
+++ training/optimizers/transformer_optimizer.py
@@ -15,13 +15,14 @@ class TransformerOptimizer(BaseOptimizer):
         possible_nheads: List[int] = [h for h in [1, 2, 4, 8] if d_model % h == 0]
         nhead = trial.suggest_categorical("nhead", possible_nheads)
         num_layers = trial.suggest_int("num_layers", 1, 4)
-        dim_feedforward = trial.suggest_categorical(
-            "dim_feedforward", [d_model * 2, d_model * 4, d_model * 8])
+        ff_multiplier = trial.suggest_categorical("ff_multiplier", [2, 4, 8])
         dropout = trial.suggest_float("dropout", 0.0, 0.5)
         seq_length = trial.suggest_int("seq_length", 5, 15)
         lr = trial.suggest_float("lr", 1e-5, 1e-2, log=True)
         epochs = trial.suggest_int("epochs", 10, 100)
         batch_size = trial.suggest_categorical("batch_size", [16, 32, 64, 128])
+
+        dim_feedforward = d_model * ff_multiplier
 
         model = self.MODEL(
             dataset=self.dataset,
