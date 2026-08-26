ef8255ee91c97b0ffecde6212b5869029b910bb0
tanhinho <115641400+tanhinho@users.noreply.github.com>
2026-08-26 23:20:42 +0100

    Add all models to init file

--- training/models/__init__.py
+++ training/models/__init__.py
@@ -1,4 +1,7 @@
 from .base_model import BaseModel
 from .lstm_model import LSTMModel
+from .random_forest_model import RandomForestModel
+from .tcn_model import TCNModel
+from .transformer_model import TransformerModel
 
-__all__ = ["BaseModel", "LSTMModel"]
+__all__ = ["BaseModel", "LSTMModel", "RandomForestModel", "TCNModel", "TransformerModel"]
